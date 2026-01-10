b0dc87be3a7fab04623564fb039e6f63c9998d49
Kevin Edry <kedry@whitepages.com>
2026-01-10 09:48:14 -0800

    refactor: remove unused code to fix compiler warnings
    - Remove unused TasksMode re-export from views/tasks/mod.rs
    - Delete unused LoadingDialog widget and loading.rs
    - Remove unused has_pending_classifications method
    - Remove unused edit_task_by_id method

--- src/widgets/dialogs/mod.rs
+++ src/widgets/dialogs/mod.rs
@@ -1,11 +1,9 @@
 mod confirm;
 mod error;
 mod input;
-mod loading;
 mod style;
 
 pub use confirm::ConfirmDialog;
 pub use error::ErrorDialog;
 pub use input::InputDialog;
-pub use loading::LoadingDialog;
 pub use style::DialogStyle;
